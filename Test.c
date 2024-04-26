c663e1438ec743d315e240bf2850ad2e1ca70b89
akha <khasiev.1999@mail.ru>
2024-04-27 01:30:08 +0700

    First task

--- Test.c
+++ Test.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void main () {
-    printf("Hello");
+    int a[3];
+    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    int min = a[0];
+    if (a[0] > a[1]) { min = a[1]; a[1] = a[0]; a[0] = min;}
+    if (a[1] > a[2]) { min = a[2]; a[2] = a[1]; a[1] = min;}
+    if (a[0] > a[1]) { min = a[1]; a[1] = a[0]; a[0] = min;} 
+    printf("%d",a[1]);
 }
